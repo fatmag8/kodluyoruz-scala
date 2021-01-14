4a57a8682cf410010cdabbdcd404c4a3611a034d
fatmag8 <fatmagezerr1997@gmail.com>
2021-01-14 18:51:28 +0300

    CsvLoader Implementation,StreetFinder implementation added,project completed.

--- src/dev/akif/kodluyoruz/streetfinder/CsvLoaderImp.scala
+++ src/dev/akif/kodluyoruz/streetfinder/CsvLoaderImp.scala
@@ -4,13 +4,13 @@ import java.io.File
 object CsvLoaderImp extends CsvLoader {
   override def loadCsv(file: File): List[String] ={
     val bufferedSource = io.Source.fromFile(file)
-    var namesList :   List[String] = {
+    var titleList :   List[String] = {
       bufferedSource.getLines()
         .map(_.split(",")
           .map(_.trim).apply(1)).toList
 
     }
     bufferedSource.close
-    namesList
+    titleList
   }
 }
